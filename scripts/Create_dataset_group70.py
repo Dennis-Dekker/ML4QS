ad4757b393ccdefd4583548db9dd50a0dbfc5879
Dennis Dekker <48018095+Dennis-Dekker@users.noreply.github.com>
2019-06-05 16:22:24 +0200

    create processed dataset

--- scripts/Create_dataset_group70.py
+++ scripts/Create_dataset_group70.py
@@ -11,13 +11,13 @@ def plot_dataset(dataset):
 
 
 def make_empty_dataset(min_t, max_t, cols, delta_t):
-    timestamps = pd.interval_range(0, max_t - min_t, freq=delta_t)
+    timestamps = np.arange(0, max_t - min_t, delta_t)
     empty_dataset = pd.DataFrame(index=timestamps, columns=cols)
 
     return empty_dataset
 
 
-def create_dataset(df_raw, delta_t):
+def create_dataset(df_raw, delta_t, labels):
     min_t = min(df_raw.time)
     max_t = max(df_raw.time)
     cols = df_raw.drop(["time"], axis=1).columns
@@ -36,8 +36,21 @@ def create_dataset(df_raw, delta_t):
             else:
                 raise ValueError("No relevant rows.")
 
+    empty_dataset = add_labels(empty_dataset, labels, delta_t, min_t, max_t)
+
     return empty_dataset
 
+def add_labels(dataset, labels, delta_t, min_t, max_t):
+    labels_list = np.unique(labels.activity)
+    for label in labels_list:
+        dataset["label_" + label] = 0
+
+    for i in range(0, len(dataset.index)):
+        for j in range(0, len(labels.index)):
+            if (i * delta_t >= labels.time_1[j]) & (i * delta_t < labels.time_2[j]):
+                dataset.loc[dataset.index[i], "label_" + labels.activity[j]] = 1
+
+    return dataset
 
 def preprocess(data):
     df_raw = data.drop(["Unnamed: 18"], axis=1)
@@ -49,15 +62,17 @@ def preprocess(data):
 
 def main():
     # Variables
-    delta_t = 0.25
+    delta_t = 0.05
 
-    data = pd.read_csv("../Data/first_data.csv")
-    labels = pd.read.csv("../Data/intervals.csv")
+    data = pd.read_csv("../data/first_data")
+    labels = pd.read_csv("../data/intervals.csv", sep="\t")
     df_raw = preprocess(data)
 
-    dataset = create_dataset(df_raw, delta_t)
+    dataset = create_dataset(df_raw, delta_t, labels)
 
-    plot_dataset(dataset)
+    print(dataset)
+
+    dataset.to_csv("../data/processed_data.csv")
 
 
 if __name__ == '__main__':
