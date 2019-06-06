917a1991d49b3dd02ae613f4700438e183297daa
Dennis Dekker <48018095+Dennis-Dekker@users.noreply.github.com>
2019-06-06 12:24:15 +0200

    new script chauvinet

---
+++ scripts/outlier_detection.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import numpy as np
+import math
+from scipy import special
+
+
+def apply_chauvinet(dataset, col):
+    mean = dataset[col].mean()
+    std = dataset[col].std()
+    N = len(dataset.index)
+    criterion = 1.0 / (2 * N)
+
+    deviation = abs(dataset[col] - mean) / std
+
+    low = -deviation / math.sqrt(2)
+    high = deviation / math.sqrt(2)
+    prob = []
+    mask = []
+
+    for i in range(0, len(dataset.index)):
+        # Determine the probability of observing the point
+        prob.append(1.0 - 0.5 * (special.erf(high.iloc[i]) - special.erf(low.iloc[i])))
+        # And mark as an outlier when the probability is below our criterion.
+        mask.append(prob[i] < criterion)
+    dataset[col[0] + '_outlier'] = mask
+
+    return dataset
+
+def load_data(file_name):
+    dataset = pd.read_csv(file_name)
+
+    return dataset
+
+def main():
+    dataset = load_data("../data/processed_data.csv")
+
+    chauvinet_cols = ["Gain"]
+
+    dataset = apply_chauvinet(dataset, chauvinet_cols)
+
+    print(dataset)
+
+    dataset.to_csv("../data/data_chauvinet_gain.csv")
+
+
+if __name__ == '__main__':
+    main()
