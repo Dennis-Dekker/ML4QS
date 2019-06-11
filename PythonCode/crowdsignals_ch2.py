c78f70206a8283cb0fb38f239e4b6e3ffc4ab61c
Martin <martinbbanchero@gmail.com>
2019-06-11 11:50:08 +0200

    lfs

--- PythonCode/crowdsignals_ch2.py
+++ PythonCode/crowdsignals_ch2.py
@@ -8,7 +8,7 @@
 ##############################################################
 
 
-dataset_path = '../datasets/crowdsignals.io/csv-participant-one/'
+dataset_path = '../data/'
 result_dataset_path = './intermediate_datafiles/'
 
 # Import the relevant classes.
