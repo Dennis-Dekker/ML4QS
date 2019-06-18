25509e979d60d3d6bda8e1f4492b8fa008942147
Dennis <dekkerdennis96@gmail.com>
2019-06-18 11:52:37 +0200

    update script mica

--- scripts/Create_datasets_Mica.py
+++ scripts/Create_datasets_Mica.py
@@ -1,9 +1,25 @@
 import pandas as pd
+import numpy as np
 import os
 from xml.dom import minidom
 import datetime
+import gpxpy
+import matplotlib.pyplot as plt
 
-def parseTrack(trk):
+
+def extract_speed(folder, gps_file):
+    gps_file = open(folder + gps_file, "r")
+    gps = gpxpy.parse(gps_file)
+    gps = gps.tracks[0].segments[0].points
+    gps_speed = []
+
+    for point_no, point in enumerate(gps):
+        gps_speed.append({"speed": point.speed_between(gps[point_no - 1])})
+
+    return gps_speed
+
+
+def parse_track(trk):
     tracks = []
     for trkseg in trk.getElementsByTagName('trkseg'):
         for trkpt in trkseg.getElementsByTagName('trkpt'):
@@ -16,19 +32,20 @@ def parseTrack(trk):
             except ValueError:
                 t = datetime.datetime.strptime(rfc3339, '%Y-%m-%dT%H:%M:%SZ')
             extensions = trkpt.getElementsByTagName('extensions')[0]
-            trkPtExtension = extensions.getElementsByTagName('gpxtpx:TrackPointExtension')[0]
-            if trkPtExtension:
-                hr = int(trkPtExtension.getElementsByTagName('gpxtpx:hr')[0].firstChild.data)
+            trkpt_extension = extensions.getElementsByTagName('gpxtpx:TrackPointExtension')[0]
+            if trkpt_extension:
+                hr = int(trkpt_extension.getElementsByTagName('gpxtpx:hr')[0].firstChild.data)
 
             tracks.append({'lat': lat, 'lon': lon, 'ele': ele, 'time': t, 'hr': hr})
 
     return tracks
 
+
 def load_data(folder, gps_file):
     doc = minidom.parse(folder + gps_file)
     gpx = doc.documentElement
     for node in gpx.getElementsByTagName("trk"):
-        tracks = parseTrack(node)
+        tracks = parse_track(node)
 
     gps_data = pd.DataFrame(tracks, columns=["time","lat", "lon", "ele", "hr"])
 
@@ -46,17 +63,31 @@ def load_location_files(folder):
 
 
 def main():
+    # folder path
     folder = "../assignment_3/Data_Mica/"
 
+    # list all file names
     gps_file_names = load_location_files(folder)
 
-    gps_data_all = pd.DataFrame(columns=["time","lat", "lon", "ele", "hr"])
+    # create empty dataframe
+    gps_data_loc = pd.DataFrame(columns=["time","lat", "lon", "ele", "hr"])
 
+    # get gps location from files, concatenate all files by rows (on date order)
     for gps_file in sorted(gps_file_names):
         gps_data = load_data(folder, gps_file)
-        print(gps_data.shape)
-        gps_data_all = gps_data_all.append(gps_data)
-    print(gps_data_all.shape)
+        gps_data_loc = gps_data_loc.append(gps_data)
+
+    gps_data_speed = pd.DataFrame(columns=["speed"])
+    for gps_file in sorted(gps_file_names):
+        gps_speed = extract_speed(folder, gps_file)
+        gps_data_speed = gps_data_speed.append(gps_speed)
+
+    gps_data_all = pd.concat([gps_data_loc, gps_data_speed], axis=1)
+
+    plt.plot(gps_data_all["time"], gps_data_all["speed"])
+    plt.show()
+
+
 
     gps_data_all.to_csv("all_data_Mica.csv",index=False)
 
