b569dd0cf3bf38d6c6143aee5a77a5fc33f687fa
Dennis <dekkerdennis96@gmail.com>
2019-06-18 13:24:17 +0200

    new scripts

---
+++ scripts/Create_dataset_WESAD.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+import os
+
+
+def load_data(path):
+
+    return data
+
+
+def main():
+    path = ""
+    data = load_data(path)
+
+
+if __name__ == '__main__':
+    main()
